f7b9c3bc06dfa13f3ec9ea7d19570e2a8806d403
YangQi0408 <a123er3643@163.com>
2025-06-21 23:55:55 +0800

    Update

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-TOOLCHAIN_PATH=$HOME/proton-clang/proton-clang-20210522/bin
+TOOLCHAIN_PATH=$HOME/kernel/toolchains/proton-clang/bin
 echo "TOOLCHAIN_PATH: [$TOOLCHAIN_PATH]"
 export PATH="$TOOLCHAIN_PATH:$PATH"
-export CCACHE_DIR="$HOME/.cache/ccache_mikernel" 
-export CC="ccache gcc"
-export CXX="ccache g++"
+export CCACHE_DIR="$HOME/.cache/ccache_p5pkernel" 
 export PATH="/usr/lib/ccache:$PATH"
 echo "CCACHE_DIR: [$CCACHE_DIR]"
 
 
-MAKE_ARGS="AS=as ARCH=arm64 SUBARCH=arm64 O=out CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- CLANG_TRIPLE=aarch64-linux-gnu-"
+MAKE_ARGS="ARCH=arm64 SUBARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- CLANG_TRIPLE=aarch64-linux-gnu-"
 
 echo "[clang --version]:"
 clang --version
 
-make $MAKE_ARGS elish_user_defconfig
+rm -rf out/
 
-make $MAKE_ARGS -j$(nproc)
+make CC="ccache clang" CXX="ccache clang++" $MAKE_ARGS elish_user_defconfig
+
+make CC="ccache clang" CXX="ccache clang++" $MAKE_ARGS -j$(nproc)
 
 sleep 2
 rm -rf out/repack; 
@@ -28,4 +28,4 @@ unzip release.zip -d out/repack
 cp out/arch/arm64/boot/Image out/repack/Image
 cd out/repack; zip -r kernel.zip *; cd ../../
 md5=$(md5sum out/repack/kernel.zip | cut -c1-8)
-mv out/repack/kernel.zip kernel_elish_$(date +%Y%m%d)_anykernel3_$md5.zip
+mv out/repack/kernel.zip anykernel3_elish_$(date +%Y%m%d)_$md5.zip
